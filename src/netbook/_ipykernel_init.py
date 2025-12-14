1167f29251d14d31bc3cd4778e9b6c9781f4ae56
Levon Haykazyan <haykazyan@zoho.com>
2025-12-14 16:26:56 +0000

    Fixed formatting of empty DataFrame.

--- src/netbook/_ipykernel_init.py
+++ src/netbook/_ipykernel_init.py
@@ -25,24 +25,29 @@ def _pd_rich_repr(self, console, console_options):
         show_dimensions=get_option("display.show_dimension"),
     )
     sformatter = StringFormatter(formatter)
-    string_cols = sformatter._get_strcols()
 
-    table = Table(box=SIMPLE, row_styles=["dim", ""])
-    if formatter.has_index_names:
-        for i, c in enumerate(string_cols):
-            table.add_column("\n".join(c[:2]), style="bold" if i == 0 else None, justify="left" if i == 0 else "right")
-        start_row = 2
+    if self.empty:
+        yield sformatter._empty_info_line.replace('[', '\[')
+
     else:
-        for i, c in enumerate(string_cols):
-            table.add_column(c[0], style="bold" if i == 0 else None, justify="left" if i == 0 else "right")
-        start_row = 1
-    for i in range(start_row, len(string_cols[0])):
-        table.add_row(*[c[i] for c in string_cols])
-    if get_option("display.expand_frame_repr"):
-        console_options = console_options.update(max_width=256)
-    yield from table.__rich_console__(console, console_options)
-    if formatter.should_show_dimensions:
-        yield formatter.dimensions_info.strip()
+        string_cols = sformatter._get_strcols()
+
+        table = Table(box=SIMPLE, row_styles=["dim", ""])
+        if formatter.has_index_names:
+            for i, c in enumerate(string_cols):
+                table.add_column("\n".join(c[:2]), style="bold" if i == 0 else None, justify="left" if i == 0 else "right")
+            start_row = 2
+        else:
+            for i, c in enumerate(string_cols):
+                table.add_column(c[0], style="bold" if i == 0 else None, justify="left" if i == 0 else "right")
+            start_row = 1
+        for i in range(start_row, len(string_cols[0])):
+            table.add_row(*[c[i] for c in string_cols])
+        if get_option("display.expand_frame_repr"):
+            console_options = console_options.update(max_width=256)
+        yield from table.__rich_console__(console, console_options)
+        if formatter.should_show_dimensions:
+            yield formatter.dimensions_info.strip()
 
 
 _DataFrame.__rich_console__ = _pd_rich_repr
